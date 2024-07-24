d124779c4be8e9fc01a05232d2eb437eb4ecd0fd
sajeshmattathil <135832263+sajeshmattathil@users.noreply.github.com>
2024-07-25 03:27:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+
+
+
+
+// Promise Chaining
+
+// const newPromise = (name, cb) => {
+//   setTimeout(() => {
+//     cb(name);
+//   }, 2000);
+// };
+// const newPromise1 = (name, cb) => {
+//   setTimeout(() => {
+//     cb(name);
+//   }, 2000);
+// };
+// const newPromise2 = (name, cb) => {
+//   setTimeout(() => {
+//     cb(name);
+//   }, 2000);
+// };
+// const newPromise3 = (name, cb) => {
+//   setTimeout(() => {
+//     cb(name);
+//   }, 2000);
+// };
+
+// newPromise("sajesh", (msg) => {
+//   console.log(msg);
+//   newPromise1("Rahul", (msg) => {
+//     console.log(msg);
+//     newPromise2("aswath", (msg) => {
+//       console.log(msg);
+//       newPromise3("arjun", (msg) => {
+//         console.log(msg);
+//       });
+//     });
+//   });
+// });
